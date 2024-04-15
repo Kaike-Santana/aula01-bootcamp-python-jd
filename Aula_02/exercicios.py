4168fcc734538dd956fde0fc394e011a62ac0fe6
Kaike Santana <kaike1010@hotmail.com.br>
2024-04-15 15:19:31 -0300

    subindo a resolucao do 2 problema

--- Aula_02/exercicios.py
+++ Aula_02/exercicios.py
@@ -1,18 +1,20 @@
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-numero_1 = int(input("Digite um número inteiro: "))
-numero_2 = int(input("Digite outro número inteiro: "))
+# numero_1 = int(input("Digite um número inteiro: "))
+# numero_2 = int(input("Digite outro número inteiro: "))
 
-soma_dos_numeros = numero_1 + numero_2
+# soma_dos_numeros = numero_1 + numero_2
 
-
-print(soma_dos_numeros)
-
-
-
-
-
+# print(soma_dos_numeros)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+number_user = int(input(f"Digite um número: "))
+
+resto_divisao = number_user % 5
+
+print(f"o resto da divisao de: {number_user}, por 5 é: {resto_divisao} ")
+
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.               
