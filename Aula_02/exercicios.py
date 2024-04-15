c6bee61ec49b753141df3535ef746d8068280486
Kaike Santana <kaike1010@hotmail.com.br>
2024-04-15 15:07:55 -0300

    subindo a resolucao do meu primeiro exercicio

---
+++ Aula_02/exercicios.py
@@ -0,0 +1,18 @@
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_1 = int(input("Digite um número inteiro: "))
+numero_2 = int(input("Digite outro número inteiro: "))
+
+soma_dos_numeros = numero_1 + numero_2
+
+
+print(soma_dos_numeros)
+
+
+
+
+
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.               
